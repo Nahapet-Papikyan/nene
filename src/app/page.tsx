cb541c05e67f700c434544101c7b0752d50c45cb
nahapet-papikyan <nahapetpapikyan@gmail.com>
2024-01-28 19:09:23 +0400

    init project

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <h1 className={styles.title}>NeNe</h1>
+        <p>Coming Soon</p>
+      </div>
+    </main>
+  );
+}
